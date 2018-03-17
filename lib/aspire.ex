bf8dfcc81cd3894e594efbee6e8da352da505be2
timCF <tim4CF@gmail.com>
2018-03-18 00:58:06 +0200

    implemented to_string and to_integer

--- lib/aspire.ex
+++ lib/aspire.ex
@@ -1,18 +1,181 @@
 defmodule Aspire do
   @moduledoc """
-  Documentation for Aspire.
+
+  Aspire functions are trivial.
+  Each function performs type conversion if it is 100% safe.
+  Else it returns first argument as is.
+
   """
 
+  defstruct [
+    decimals: nil,
+    compact: true,
+  ]
+
   @doc """
-  Hello world.
+
+  Safe conversion to string (binary) type.
+  Second argument is %Aspire{} structure with options
+
+    - decimals: non negative integer, required if first argument is float
+    - compact: boolean, optional, default true
 
   ## Examples
 
-      iex> Aspire.hello
-      :world
+      ```
+      iex> Aspire.to_string(:hello)
+      "hello"
+      iex> Aspire.to_string(MyApp.Mymodule)
+      "Elixir.MyApp.Mymodule"
+      iex> Aspire.to_string(nil)
+      "nil"
+      iex> Aspire.to_string(:undefined)
+      "undefined"
+      iex> Aspire.to_string(false)
+      "false"
+
+      iex> Aspire.to_string(0)
+      "0"
+      iex> Aspire.to_string(123)
+      "123"
+      iex> Aspire.to_string(-123)
+      "-123"
+
+      iex> Aspire.to_string(0.00000, %Aspire{decimals: 8})
+      "0.0"
+      iex> Aspire.to_string(1.230000, %Aspire{decimals: 8})
+      "1.23"
+      iex> Aspire.to_string(1.230000, %Aspire{decimals: 8, compact: false})
+      "1.23000000"
+      iex> Aspire.to_string(1.23e5, %Aspire{decimals: 8})
+      "123000.0"
+
+      iex> Aspire.to_string('hello world')
+      "hello world"
+      iex> Aspire.to_string([])
+      ""
+      iex> Aspire.to_string([12345, 12345, 12345])
+      [12345, 12345, 12345]
+      iex> Aspire.to_string([hello: "world"])
+      [hello: "world"]
+
+      iex> Aspire.to_string(%{hello: "world"})
+      %{hello: "world"}
+      ```
 
   """
-  def hello do
-    :world
+
+  def to_string(some, params \\ %__MODULE__{})
+
+  def to_string(atom, %__MODULE__{}) when is_atom(atom) do
+    atom
+    |> Atom.to_string
   end
+
+  def to_string(integer, %__MODULE__{}) when is_integer(integer) do
+    integer
+    |> Integer.to_string
+  end
+
+  def to_string(float, %__MODULE__{decimals: decimals, compact: compact})
+                  when
+                    is_float(float) and
+                    is_integer(decimals) and
+                    (decimals >= 0) and
+                    is_boolean(compact)
+                  do
+    opts =
+      case compact do
+        true -> [:compact]
+        false -> []
+      end
+      |> Kernel.++([decimals: decimals])
+
+    float
+    |> :erlang.float_to_binary(opts)
+  end
+
+  def to_string(list, %__MODULE__{}) when is_list(list) do
+    try do
+      :erlang.list_to_binary(list)
+    rescue
+      _ -> list
+    catch
+      _, _ -> list
+    end
+  end
+
+  def to_string(some, _), do: some
+
+  @doc """
+
+  Safe conversion to integer type.
+
+  ## Examples
+
+      ```
+      ```
+
+  """
+
+  def to_integer(binary) when is_binary(binary) do
+    binary
+    |> Integer.parse
+    |> case do
+      {integer, ""} ->
+        integer
+      _ ->
+        binary
+        |> Float.parse
+        |> case do
+          {float, ""} ->
+            float
+            |> __MODULE__.to_integer
+            |> case do
+              integer when is_integer(integer) -> integer
+              _ -> binary
+            end
+          _ ->
+            binary
+         end
+    end
+  end
+
+  def to_integer(float) when is_float(float) do
+    float
+    |> round
+    |> case do
+      integer when (float == integer) -> integer
+      _ -> float
+    end
+  end
+
+  def to_integer(atom) when is_atom(atom) do
+    atom
+    |> Atom.to_string
+    |> __MODULE__.to_integer
+    |> case do
+      integer when is_integer(integer) -> integer
+      _ -> atom
+    end
+  end
+
+  def to_integer(list) when is_list(list) do
+    list
+    |> __MODULE__.to_string
+    |> case do
+      list when is_list(list) ->
+        list
+      binary when is_binary(binary) ->
+        binary
+        |> to_integer
+        |> case do
+          integer when is_integer(integer) -> integer
+          _ -> list
+        end
+    end
+  end
+
+  def to_integer(some), do: some
+
 end
--- test/aspire_test.exs
+++ test/aspire_test.exs
@@ -1,8 +1,4 @@
 defmodule AspireTest do
   use ExUnit.Case
   doctest Aspire
-
-  test "greets the world" do
-    assert Aspire.hello() == :world
-  end
 end
